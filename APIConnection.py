6ece152771613309078eaf81af850185efaf82c1
Andres Restrepo <aorestrepor@correo.udistrital.edu.co>
2020-11-29 19:40:42 -0500

    Add classes

---
+++ APIConnection.py
@@ -0,0 +1,52 @@
+import tweepy
+from tweepy import OAuthHandler
+
+class APIConnection():
+
+    def __init__(self):
+        self._customerKey = ""
+        self._customerSecret = ""
+        self._accessToken = ""
+        self._accessTokenSecret = ""
+        self._auth = None
+        self._connection = None
+
+    def setCustomerKey(self, customerkey):
+        self._customerKey = customerkey
+
+    def setCustomerSecret(self, customersecret):
+        self._customerSecret = customersecret
+
+    def setAccessToken(self, accesstoken):
+        self._accessToken = accesstoken
+
+    def setAccessTokenSecret(self, accesstokensecret):
+        self._accessTokenSecret = accesstokensecret
+
+    def setAuthHandler(self, customerkey, customersecret):
+        self._auth = OAuthHandler(customerkey, customersecret)
+
+    def setConnection(self, auth, accesstoken, accesstokensecret, connection):
+        if connection is None:
+            auth.set_access_token(accesstoken, accesstokensecret)
+            self._connection = tweepy.API(auth)
+        else:
+            self._connection = connection
+
+    def getCustomerKey(self):
+        return self._customerKey
+
+    def getCustomerSecret(self):
+        return self._customerSecret
+
+    def getAccessToken(self):
+        return self._accessToken
+
+    def getAccessTokenSecret(self):
+        return self._accessTokenSecret
+
+    def getAuthHandler(self):
+        return self._auth
+
+    def getConnection(self):
+        return self._connection
